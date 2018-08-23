3855e5cf9e757f458128d017441be2862f798b8d
Kuba Przetakiewicz <Jakub.Przetakiewicz@IGT.com>
2018-08-23 15:11:51 +0200

    FIX dist build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './js/main.js',
+  entry: "./js/main.js",
   output: {
-    path: path.resolve(__dirname, 'js'),
-    filename: './js/out.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/out.js"
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          query: { 
-            presets: ['env', 'stage-2', 'react']
+          loader: "babel-loader",
+          query: {
+            presets: ["env", "stage-2", "react"]
           }
         }
       },
-      { 
+      {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "resolve-url-loader", "sass-loader"]
       },
       {
         test: /\.(png|jpg|svg|gif|webp)/,
-        use: 'file-loader'
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/"
+          }
+        }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: "./index.html"
     })
   ],
   devServer: {
